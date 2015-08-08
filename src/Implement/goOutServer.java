090da73f955650d2bfaab326628d8233a2780a1d
pmatusse <paulo.matusse@gmail.com>
2015-08-08 18:05:52 +0200

    depois de listar eventos por administrador

--- src/Implement/goOutServer.java
+++ src/Implement/goOutServer.java
@@ -1,9 +1,12 @@
 
 package Implement;
 
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.RMISecurityManager;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 
 public class goOutServer {
@@ -12,20 +15,35 @@ public class goOutServer {
     public static void main(String args[]){
        /* System.getProperties().put("java.security.policy", "security.policy");
         System.setSecurityManager(new RMISecurityManager());*/
+        ServerSocket server = null;
         try {
-            
             
             Registry reg = LocateRegistry.createRegistry(1099);
             GoOutServerImpl _goOutServer = new GoOutServerImpl();
             reg.rebind("goOutServer", _goOutServer);
             System.out.println("Servidor a escuta...");
             
-        } catch (Exception e) {
             
+            
+            
+            //tratar tcp
+            server = new ServerSocket(1111);
+            System.out.println("Server is now runnign at  port 1111");
+            
+            while (true) {
+			try {
+				Socket clientSocket = server.accept();
+		/*		Client client = new Client(clientSocket);
+
+				Thread clientThread = new Thread(client);
+				clientThread.start();*/
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        
-    
+        }    
     }
     
 }
