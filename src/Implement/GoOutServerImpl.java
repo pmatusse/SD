0c0ed54061761757b466cf148b40b8d025ad75f1
pmatusse <paulo.matusse@gmail.com>
2015-08-08 18:05:59 +0200

    depois de listar eventos por administrador

--- src/Implement/GoOutServerImpl.java
+++ src/Implement/GoOutServerImpl.java
@@ -7,20 +7,27 @@ import Model.Tabelas;
 import java.util.ArrayList;
 import sun.util.calendar.BaseCalendar.Date;
 import Model.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Vector;
 
 public class GoOutServerImpl extends UnicastRemoteObject implements goOutServerInterface {
 
     //tratamento da conexao tcp
-    
-    
-    
-    
+    ServerSocket myServerSocket;
+    boolean ServerOn = true;
+    boolean tcp = false;
+
     Tabelas tabelas = new Tabelas();
     public String adminActivo; //armazena o e-mail do admin que esta activo no momento
-    //constructor
 
+    //constructor
     GoOutServerImpl() throws RemoteException {
+        if (tcp == true) {
+
+
+        }
     }
 
     @Override
@@ -32,17 +39,16 @@ public class GoOutServerImpl extends UnicastRemoteObject implements goOutServerI
         return false;
     }
 
- 
     public Vector ListarEventosAdmin(String mail) throws RemoteException {
         Vector tEventos = new Vector();
         for (Event evento : tabelas.Events) {
             if (evento.getMailCriador().equalsIgnoreCase(mail)) {
                 //evento localizado , sera impresso
-                    tEventos.add(evento);
+                tEventos.add(evento);
             }
         }
         return tEventos;
-        
+
     }
 
     @Override
@@ -51,30 +57,30 @@ public class GoOutServerImpl extends UnicastRemoteObject implements goOutServerI
         for (Event evento : tabelas.Events) {
             if (evento.getTitulo().equalsIgnoreCase(titulo)) {
                 //evento localizado , sera impresso
-                    tEventos.add(evento);
+                tEventos.add(evento);
             }
         }
         return tEventos;
     }
 
     @Override
-    public void removerEvento(String titulo) throws RemoteException {
+     public boolean removerEvento(String titulo) throws RemoteException {
         for (Event evento : tabelas.Events) {
             if (evento.getTitulo().equalsIgnoreCase(titulo)) {
                 //evento localizado , removido
                 tabelas.Events.remove(evento);
+                return  true;
+            }else{
+            return false;
             }
-        }
-        
+         }
+        return removerEvento(titulo);
+     
     }
 
     @Override
     public boolean RegistrarUser(String mail, String psw, String nome, int telemovel) throws RemoteException {
-        
-        
-        
-        
-        
+
         return false;
     }
 
@@ -113,7 +119,7 @@ public class GoOutServerImpl extends UnicastRemoteObject implements goOutServerI
 
     @Override
     public boolean AutenticarAdmin(String mail) throws RemoteException {
-       
+
         if (mail.equalsIgnoreCase(mail)) {
             return true;
         }
@@ -123,14 +129,19 @@ public class GoOutServerImpl extends UnicastRemoteObject implements goOutServerI
 
     @Override
     public String getAdminactivo() throws RemoteException {
-       
-        System.out.println("adminiActivo"+adminActivo);
-      return adminActivo;
+
+        System.out.println("adminiActivo" + adminActivo);
+        return adminActivo;
     }
 
     @Override
     public void setAdminactivo(String adminemail) throws RemoteException {
-      adminActivo=adminemail;
+        adminActivo = adminemail;
     }
+
+
+
+
+
 
 }
