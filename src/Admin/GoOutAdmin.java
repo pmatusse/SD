a7286e1b7032eb1cfa8be51e72bf7477350216b6
begasgab <begasgab@gmail.com>
2015-08-09 03:26:57 -0700

    menus do administrador

--- src/Admin/GoOutAdmin.java
+++ src/Admin/GoOutAdmin.java
@@ -1,5 +1,6 @@
 package Admin;
 
+
 import Interfaces.goOutServerInterface;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,83 +16,153 @@ public class GoOutAdmin {
 
     public static Registry adminReg;
     public static goOutServerInterface admin;
+    private static Scanner scan = new Scanner(System.in);
+    private static String email, password, nome;
+    private static int telemovel;
+    private static String titulo, descricao, local, data, mailCriador;
 
     public static void main(String[] args) throws RemoteException {
+        menuPrincipal();
+    }
 
+    /*Inicia o meu principa;*/
+    public static void menuPrincipal() {
+        int opcao = 0;
+        System.out.println("****BEM VINDO A GOOUT EVENT****");
+        System.out.println("********1 --> login************");
+        System.out.println("********2 --> sair*************");
+        System.out.println("********GOOUT EVENT************");
+
+        opcao = scan.nextInt();
+        operacoes1(opcao);
+    }
+    /*menu das operacoes de administrador*/
+
+    public static void menuAdministrado() throws RemoteException {
+        int opcao = 0;
+        System.out.println("\n");
+        System.out.println("1 --> Adicionar user's");
+        System.out.println("2 --> Adicionar evento");
+        System.out.println("3 --> Listar Eventos por administradores");
+        System.out.println("4 --> Ver Detalhes de evento");
+        System.out.println("5 --> Remover evento");
+        System.out.println("6 --> Menu Principla");
+        System.out.println("7 --> Sair");
+        opcao = scan.nextInt();
+        operacoesaAdministrador(opcao);
+
+    }
+
+    public static void operacoes1(int operacao) {
         try {
-
             adminReg = LocateRegistry.getRegistry("127.0.0.1", 1099);
             admin = (goOutServerInterface) adminReg.lookup("goOutServer");
             int op, tentativas = 3;
-            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
             Scanner in;
             boolean loop = true;
 
-            do {
-                System.out.print("Digite seu e-mail: ");
-                String email = stdin.readLine();
-                //autenticar administrador
-                if (new GoOutAdmin().validEmail(email)) {
-                    System.out.println("Bem vindo ao goOut");
 
+            switch (operacao) {
+                case 1:
+                    System.out.print("Digite seu e-mail: \n");
+                    String email = scan.next();
+                    //autenticar administrador
+                    if (new GoOutAdmin().validEmail(email)){
+                         
+                         System.out.println("Bem vindo ao goOut:" + email);
+                        admin.setAdminactivo(email);
+                        menuAdministrado();
+                         
+                        }else
+                        System.out.println("O usuario nao existe");
+                        menuPrincipal();
+                    
 
-                    while (loop) {
-                        // visualizar menu
-                        System.out.println("1 --> Adicionar evento");
-                        System.out.println("2 --> Listar Eventos");
-                        System.out.println("3 --> Ver Detalhes de evento");
-                        System.out.println("4 --> Remover evento");
-                        System.out.println("Digite sua opcao:");
-                        in = new Scanner(System.in);
+                    break;
+                case 2:
+                    System.out.println("GOODBYE VOLTE SEMPRE");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcao nao valida");
 
-                        //capturar escolha
-                        op = in.nextInt();
-                        switch (op) {
-                            case 1:
-                                if (admin.addEvento("Festa da Coca-cola",
-                                        "Festa alusiva ao aniversario da coca-cola",
-                                        "Campos do Isctem",
-                                        "13/9/2015")) {
-                                    
-                                    System.out.println("evento adicionado com sucesso");
-                                }
-                                else{
-                                    System.out.println("ERRO! evento nao adicionado");
-                                }
-                                break;
-                                
-                                
-
-                            case 2:
-                                break;
-
-                            case 3:
-                                admin.ListarEventosAdmin(email);
-                                break;
-
-                            case 0:
-                                loop = false;
-                                break;
-
-                            default:
-                                System.out.println("opcao invalida");
-                        }
-                    }
-
-
-                } else {
-
-                    System.out.println("Accesso negado");
-                }
-            } while ((tentativas--) > 0);
-
-            System.out.println("Adeus, volte sempre");
-
+                    menuPrincipal();
+                    break;
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Ocorreu  um erro \n exception: " + e.getMessage());
         }
+        //invoca o menu
+        menuPrincipal();
+    }
 
+    public static void operacoesaAdministrador(int op) throws RemoteException {
 
+        try {
+            adminReg = LocateRegistry.getRegistry("127.0.0.1", 1099);
+            admin = (goOutServerInterface) adminReg.lookup("goOutServer");
+            //capturar escolha
+            System.out.println(op);
+            switch (op) {
+                case 1:
+                   
+                    System.out.println("Introduza a password:\n");
+                    password = scan.next();
+
+                    System.out.println("Introduza o nome : \n");
+                    nome = scan.next();
+
+                    System.out.println("Introduza o numero de telefone:\n");
+                    telemovel = scan.nextInt();
+                    if (admin.RegistrarUser(email, nome, password, telemovel)) {
+                        System.out.println("Registado novo user:\t Emai:" + email);
+                        menuAdministrado();
+                    } else {
+                        System.out.println("Usuario nao cadastrado");
+                        menuAdministrado();
+                    }
+                    break;
+
+                case 2:
+//                    (String titulo, String descricao, String local, String data, String mailCriador)
+                    System.out.println("Introduza o titulo do evento:\n");
+                    titulo = scan.next();
+                    System.out.println("Introduza a descricao:\n");
+                    descricao = scan.next();
+
+                    System.out.println("Introduza o local: \n");
+                    local = scan.next();
+
+                    System.out.println("Introduza a data:\n");
+                    data = scan.next();
+                    if (admin.addEvento(titulo, descricao, local, data)) {
+                        System.out.println("Registado novo Evento:\t Evento:" + titulo);
+                        menuAdministrado();
+                    } else {
+
+                        System.out.println("O evento nao foi addicionado");
+                    }
+                    break;
+                case 3:
+                    System.out.println("listar Eventos");
+                    admin.lista(admin.getAdminactivo());
+                        //menuAdministrado();
+                    
+                    break;
+
+                case 4:
+                    System.out.println("ola entrei no detalhes do  eventos");
+
+                    //   loop = false;
+                    break;
+
+                default:
+                    System.out.println("opcao invalida");
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu  um erro \n exception: " + e.getMessage());
+        }
     }
 
     public boolean validEmail(String email) {
